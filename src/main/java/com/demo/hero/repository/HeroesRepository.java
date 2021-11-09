62384d6e6fe0fcf2835df57426d72f67ee27b8a3
Maike Rodrigues <maikerodrigues@cialne.com.br>
2021-11-09 19:21:22 -0300

    Finalizado livecode webflux

---
+++ src/main/java/com/demo/hero/repository/HeroesRepository.java
@@ -0,0 +1,9 @@
+package com.demo.hero.repository;
+
+import com.demo.hero.document.Heroes;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface HeroesRepository extends CrudRepository<Heroes, String>{
+}
